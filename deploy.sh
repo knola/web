c1abf2a7a9d97b3eaef54ee58c14e0e7dbb1f4bf
codykung <cody@irigwe.local>
2018-01-06 10:44:39 -0800

    Deployment script for app
    This is merely a simplicity wrapper around gcloud.

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Push the app to Google App Engine
+
+if ! type gcloud > /dev/null; then
+  echo "Please set up gcloud first: https://cloud.google.com/sdk/docs/quickstarts"
+  exit 1
+fi
+
+gcloud app deploy app.yaml --project elliptical-tree-187121
